23c5e0f3c63e65f857eea1afd1737d602695c52a
Owen Campbell <owen.campbell@tanti.org.uk>
2022-06-17 16:39:36 +0100

    Handle anvil css substitutions

--- {{cookiecutter.project_name}}/docker/theme_builder/build.sh
+++ {{cookiecutter.project_name}}/docker/theme_builder/build.sh
@@ -4,5 +4,11 @@ OUTPUT_FILE="/app/${APP_NAME}/theme/assets/theme.css"
 
 mkdir /build/src
 cp -r /theme/* /build/src
-postcss src/index.css -o ${OUTPUT_FILE}
+find -type f -exec sed -i -r 's/(%color:.*%)/"--substitution--\1--substitution--";/' {} \;
+find -type f -exec sed -i 's/%anvil-banner-height%/var(--anvil-banner-height)/' {} \;
+postcss src/index.css -o theme.css
+sed -i 's/"--substitution--//g' theme.css
+sed -i 's/--substitution--"//g' theme.css
+sed -i 's/var(--anvil-banner-height)/%anvil-banner-height%/g' theme.css
+mv theme.css ${OUTPUT_FILE}
 rm -rf build/src
